c7c0eba1c0eeabb7ef5f9471fb5b0378ef37be2e
Matt March <matthewmarch29@gmail.com>
2019-03-17 17:35:08 +0000

    Call lifx script with cron job

--- wake_up.py
+++ wake_up.py
@@ -1,4 +1,10 @@
 import datetime
+import lifx
+
+# Turns on slowly over 5 minutes
+TURN_ON_PERIOD = 5 * 60
+
 
 if __name__ == '__main__':
-    print(f'Script was run at {datetime.datetime.now()}')
+    print(f'Wake up script was run at {datetime.datetime.now()}')
+    lifx.ramp_on(30)
